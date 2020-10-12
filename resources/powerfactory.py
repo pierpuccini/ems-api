b0244195c54776cc848ebda7abacd01784b5b85f
pierpuccini <pierpuccini@gmail.com>
2020-10-12 18:12:29 -0500

    updated debug config

--- resources/powerfactory.py
+++ resources/powerfactory.py
@@ -119,25 +119,4 @@ class LoadFlow(Resource):
         prj[int(project)].Deactivate()
 
         return Response(dumps(parsed_response), mimetype="application/json", status=200)
-    # def get(self):
-    #   movies = Movie.objects().to_json()
-    #   return Response(movies, mimetype="application/json", status=200)
-    # def post(self):
-    #   body = request.get_json()
-    #   movie = Movie(**body).save()
-    #   id = movie.id
-    #   return {'id': str(id)}, 200
-    #
-    # class MovieApi(Resource):
-    #   def put(self, id):
-    #     body = request.get_json()
-    #     Movie.objects.get(id=id).update(**body)
-    #     return '', 200
-    #
-    #   def delete(self, id):
-    #     movie = Movie.objects.get(id=id).delete()
-    #     return '', 200
-    #
-    #   def get(self, id):
-    #     movies = Movie.objects.get(id=id).to_json()
-    #     return Response(movies, mimetype="application/json", status=200)
+
