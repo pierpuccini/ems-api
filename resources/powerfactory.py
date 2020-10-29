ff592c7cfe6a4461137cbc48299de5c652ad69d5
pierpuccini <pierpuccini@gmail.com>
2020-10-29 03:02:30 -0500

    fix

--- resources/powerfactory.py
+++ resources/powerfactory.py
@@ -201,7 +201,7 @@ class LoadFlow(Resource):
 
         return Response(dumps(parsed_response), mimetype="application/json", status=200)
 
-    def post(self, project, elem_type):
+    def post(self, project, elem_type, tension_type):
         pf_app = pf.GetApplication()
         if pf_app is None:
             # raise Exception("getting PowerFactory application failed")
